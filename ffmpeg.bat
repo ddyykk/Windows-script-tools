99a8700c66095a91756d18421653a14f75916346
Johnson Du <42371034+ddyykk@users.noreply.github.com>
2023-06-30 09:14:00 +0800

    Update ffmpeg.bat
    Change the comment from :: to rem, because it is causing some system error for no reason. And put a pause after the job is done so user can see the result.

--- ffmpeg.bat
+++ ffmpeg.bat
@@ -28,15 +28,18 @@ if "!option!"=="1" (
     echo:
     echo "Your file is !filepath!"
     set /p "angle=Please enter the rotation angle (0 = 90 CounterCLockwise and Vertical Flip, 1 = 90 Clockwise, 2 = 90 CounterClockwise, 3 = 90 Clockwise and Vertical Flip): "
-    echo !angle!
-    :: this calls a function to pass the input file name to the default output file.
-    :: the quotation mark is important otherwise file name cannot have any space in it.
+    rem I have to use rem for comment, using :: is causing the system having error for unknow reason
+    rem echo !angle!
+    rem echo !filepath!
+    rem this calls a function to pass the input file name to the default output file.
+    rem the quotation mark is important otherwise file name cannot have any space in it.
     call :getfilename "!filepath!"
     set /p "outputname=Please enter the output file name or path (default is !outputname!): "
     echo "Your outputname is !outputname!"
     ::this is for debug use
-    ::echo .\ffmpeg -i "!filepath!" -vf "transpose=!angle!" "!outputname!"
+    echo .\ffmpeg -i "!filepath!" -vf "transpose=!angle!" "!outputname!"
     .\ffmpeg -i "!filepath!" -vf "transpose=!angle!" "!outputname!"
+    timeout /t -1
 )
 if "!option!"=="2" (
     :input_path2
@@ -50,15 +53,16 @@ if "!option!"=="2" (
     call :getfilename "!filepath!"
     set /p outputname="Please enter the output file name or path (default is !outputname!): "
     .\ffmpeg -i "!filepath!" -ss !start! -to !end! -c copy "!outputname!"
+    timeout /t -1
 )
 if "!option!"=="3" (
     exit
 )
 goto begin
 
-::The ~nx modifier is used to extract the filename and extension from a file path variable. 
-::However, it only works directly on variables that are parameters to a script or function, not on arbitrary variables.
-::The 1 here means the first arguement that passed in, which is !filepath!.
+rem The ~nx modifier is used to extract the filename and extension from a file path variable. 
+rem However, it only works directly on variables that are parameters to a script or function, not on arbitrary variables.
+rem The 1 here means the first arguement that passed in, which is !filepath!.
 :getfilename
 set "outputname=%~nx1"
 goto :eof
